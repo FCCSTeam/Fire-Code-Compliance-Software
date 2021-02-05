5f5cae3f267d248aac21a91bdb8a30ebfba9512e
DamanB <damanbharaj@gmail.com>
2021-02-05 17:37:53 -0500

    updated connection

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCrUI3NOJol7lVLx9VlLf9fIjsRHgzNY4M",
-    authDomain: "fire-code-compliance-software.firebaseapp.com",
-    projectId: "fire-code-compliance-software",
-    storageBucket: "fire-code-compliance-software.appspot.com",
-    messagingSenderId: "1085587302993",
-    appId: "1:1085587302993:web:5b0ff2340dccfe30152670",
-    measurementId: "G-48Q5X5PBEE"
-  };
+  apiKey: "AIzaSyCrUI3NOJol7lVLx9VlLf9fIjsRHgzNY4M",
+  authDomain: "fire-code-compliance-software.firebaseapp.com",
+  projectId: "fire-code-compliance-software",
+  storageBucket: "fire-code-compliance-software.appspot.com",
+  messagingSenderId: "1085587302993",
+  appId: "1:1085587302993:web:a4afba4eafa24ccd152670",
+  measurementId: "G-7EPQK3G2SK"
+};
 
   //init firebase
   firebase.initializeApp(firebaseConfig)
