439220aa12a62f1cd708ba61fb26aad064b8fd48
DamanB <damanbharaj@gmail.com>
2021-02-12 18:53:25 -0500

    added client side login form verification

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -1,10 +1,19 @@
 <template>
   <form @submit.prevent="Login">
     <h3>Sign In</h3>
-    <input type="email" required v-model="email" placeholder="Email" />
-    <input type="password" required v-model="password" placeholder="Password" />
-    <button>Login</button>
-    <div v-if="error">{{ error }}</div>
+    <div id="login-email">
+      <label for="email">Email: </label>
+      <input type="email" required v-model="email" placeholder="Email" />
+      <div class="error">{{ emailError }}</div>
+    </div>
+    <div id="login-password">
+      <label for="password">Password: </label>
+      <input type="password" required v-model="password" placeholder="Password"/>
+    </div>
+    <div id="login-submit">
+      <button>Login</button>
+      <div class="error">{{ submitError }}</div>
+    </div>
   </form>
 </template>
 
@@ -17,26 +26,47 @@ export default {
     return {
       email: "",
       password: "",
-      error: null,
+      emailReg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+      submitError: null,
+      emailError: null,
     };
   },
   methods: {
     Login() {
-      userLogin(this.email, this.password).then((res) => {
-        if (res.error) {
-          this.error = res.error;
-        } else if (res.user) {
-          //successful login
-          this.$router.replace({ name: "Userpage" });
-        } else {
-          //something went wrong with getting the user from firebase
-          this.error = "Something went wrong trying to login";
-        }
-      });
+      var noFormErrors = true;
+      this.emailError = null;
+      this.submitError = null;
+
+      //client side verification
+      if (!this.email) {
+        this.emailError = "An email is required";
+        noFormErrors = false;
+      } else if (!this.emailReg.test(this.email)) {
+        this.emailError = "Enter a valid email address";
+        noFormErrors = false;
+      }
+      
+      //send request to server if client side is good
+      if (noFormErrors) {
+        userLogin(this.email, this.password).then((res) => {
+          if (res.error) {
+            this.submitError = res.error;
+          } else if (res.user) {
+            //successful login
+            this.$router.replace({ name: "Userpage" });
+          } else {
+            //something else went wrong with getting the user from firebase
+            this.submitError = "Something went wrong trying to login";
+          }
+        });
+      }
     },
   },
 };
 </script>
 
 <style>
+.error{
+    color: red;
+}
 </style>
