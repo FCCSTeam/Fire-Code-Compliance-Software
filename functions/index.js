c47c587ed91c8e3271ef7d8346f3267ed7ad4880
DamanB <damanbharaj@gmail.com>
2021-02-16 16:01:18 -0500

    added adminRole function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    //get user and add custom admin claim
+    return admin.auth().getUserByEmail.getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin: true
+        });
+    }).then(() => {
+        message: `Success! ${data.email} is now an administrator`
+    }).catch(err => {
+        return err;
+    });
+});
