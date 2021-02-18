e0486b4c5295aa12c8ee8f54839d779018eb7e0a
willh-1 <william.h5899@gmail.com>
2021-02-18 15:33:32 -0800

    navbar changes

--- src/components/navbar/NavBar1.vue
+++ src/components/navbar/NavBar1.vue
@@ -4,13 +4,6 @@
       <b-navbar-brand>
         FCCS
       </b-navbar-brand>
-      <b-navbar-toggle>
-        <b-navbar-nav right>
-          <b-nav-item href="#">Home</b-nav-item>
-        </b-navbar-nav>
-      </b-navbar-toggle>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <!-- <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
