3c6c99e3389629d4097336c8535a870b13b81dee
DamanB <damanbharaj@gmail.com>
2021-02-22 16:57:08 -0500

    added navbar to home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="home">
+    <Navbar1 />
     <Login />
   </div>
 </template>
 
 <script>
+import Navbar1 from "@/components/navbar/NavBar1.vue";
 import Login from '@/components/authentication/Login.vue'
 export default {
   name: 'Home',
   components: {
+    Navbar1,
     Login
   }
 }
