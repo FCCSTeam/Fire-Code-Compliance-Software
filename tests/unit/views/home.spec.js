d5e2739c02d9932a65bf1ca2e1eba05a7d73d08a
DamanB <damanbharaj@gmail.com>
2021-03-09 17:30:25 -0500

    added more test cases

--- tests/unit/views/home.spec.js
+++ tests/unit/views/home.spec.js
@@ -8,12 +8,16 @@ import Navbar from '@/components/prefabs/navbar/Navbar.vue'
 import LoginModal from "@/components/authentication/ModalLogin.vue";
 import { BButton, BModal } from 'bootstrap-vue';
 
-describe('Home.vue Renders components: ', () => {
+describe('Home.vue', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallowMount(Home, { localVue });
   });
+
+  it('Correct web page rendered', () => {
+    expect(wrapper.find("#home").exists()).to.be.true;
+  });  
 
   it('Navbar renders', () => {
     expect(wrapper.findComponent(Navbar).exists()).to.be.true;
@@ -27,22 +31,14 @@ describe('Home.vue Renders components: ', () => {
     expect(wrapper.findComponent(LoginModal).exists()).to.be.true;
   });  
 
-});
-
-describe('Home.vue Functionality: ', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = mount(Home, { localVue, attachTo: document.getElementById('root') });
-  });
-
   it('Login modal appears when login button clicked', async () => {
+    wrapper = mount(Home, { localVue });
     const button = wrapper.findComponent(BButton);
     const modal = wrapper.find("#loginModal");
     
     expect(modal.isVisible()).to.be.false;
     await button.trigger('click');
     expect(modal.isVisible()).to.be.true;
-  });  
-
+  }); 
 });
+
