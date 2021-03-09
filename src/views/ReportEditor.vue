298cdec63ad201e329514d53d706d9496534c582
akarjun8147 <kaushik.81@hotmail.com>
2021-03-09 17:16:10 -0500

    added functionality to pass json to report editor

--- src/views/ReportEditor.vue
+++ src/views/ReportEditor.vue
@@ -1,14 +1,37 @@
 <template>
+<div>
     <Navbar />
-
+</div>
 </template>
 
 <script>
 import Navbar from "@/components/prefabs/navbar/Navbar.vue";
+import { getFile } from "@/js/auth/data/storeFile.js";
 
 export default {
     name: "ReportEditor",
-    components: {Navbar}
+    components: {Navbar},
+  data() {
+    return {
+
+    };
+  },
+  //mounted to set local/storage with file
+  //use computed to return something from method --> get file from local storage
+  mounted()
+  {
+
+  },
+  computed()
+  {
+     
+  },
+  methods: 
+  {
+      
+  }
+
+
 
 }
 </script>
