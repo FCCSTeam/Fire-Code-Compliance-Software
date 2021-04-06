5262ebb2955b5a0da5161cd354df45319737e2b3
willh-1 <william.h5899@gmail.com>
2021-04-06 12:14:49 -0700

    Type 3 legends/notes above the tables

---
+++ src/components/reporteditor/standards/Type3Code_legends/AFAS_Devices.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <h3>
+          Fire Alarm Monthly Testing Record	
+      </h3>
+      <h4>
+        Individual Device Record
+        <br>
+        (Reference : Clauses 6.6.4.1.4, 6.6.8.1.1-B, CAN/ULC-S536-97)
+      </h4>
+      <p>
+        <b>Notes</b>
+        <br>	
+        <b>A</b>	Correctly installed
+        <br>
+        <b>B1</b>	Missing
+        <br>
+        <b>B2</b>	Requires Service or Repairs
+        <br>
+        <b>C</b>	Alarm Operation Confirmed
+        <br>
+        <b>D</b>	Annunciation Indication Confirmed
+        <br>
+        <b>E</b>	Circuit Number Or Address
+        <br>
+        <b>F</b>	Supervision and Ground Fault Detection of Wiring to Device Confirmed (Not Required Monthly)
+        <br>
+        <b>G</b>	Smoke Sensitivity Testing (Not Required Monthly)
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/reporteditor/standards/Type3Code_legends/AFAS_Monthly2020.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <h3>
+          Fire Alarm Monthly Test and Inspection Record (Reference Clauses 4.7 and  5.2.1, CAN/ULC-S536-13)
+      </h3>
+      <h4>
+          Legend
+      </h4>
+      <p>
+            1A.  System Trouble Signal Indicator 'OFF'.
+            <br>
+            1B.  Power Indication Lamp 'ON'.
+            <br>
+            2A.  Inspection of one initiating field device - Rotational
+            <br>
+            2B.  Alert/Alarm Signal confirmed - Rotational
+            <br>
+            2C.  Annunciator:  Tested device annunciates 
+            <br>
+            2D.  Audible and visual trouble signal  
+            <br>
+            3A.  Confirmation of alarm trransmission
+            <br>
+            4A.  Test (rotational):  One emergency telephone for two-way communication and indication
+            <br>
+            4B.  Test (rotational):  Voice paging to one zone.
+            <br>
+            *  Fire Code regulations require that these records be retained a minimum of two years. 
+            <br>
+            *  Tests to be conducted in accordance with CAN/ULC-S536 and Ontario Fire Code.
+            <br>
+            *  The monthly test is not required during the month  that the annual test is completed.
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
